92c5f92e0cfb4140ea96a9e1b2eced2cf0c59abd
NicolasM <nicolas.malgat@hotmail.fr>
2021-01-08 21:10:24 +0100

    Ajout de la fonctionnalite sauvegarde modele et test sur une image

--- modules/observation_modele.py
+++ modules/observation_modele.py
@@ -15,14 +15,16 @@ def plot_layer(model: Model, test_image: str, layer_index: int, cmap='plasma'):
     img_arr = np.expand_dims(img_arr, axis=0)
 
     activation_model = Model(inputs=model.input, outputs=layer_outputs)
-    activations = activation_model.predict(img_arr)
+    activation = activation_model.predict(img_arr)[layer_index]
+    title = model.layers[layer_index].name
     
-    __display_activation(activations, 8, 4, layer_index, cmap=cmap)
+    __display_activation(activation, title, 8, 4, cmap=cmap)
 
-def __display_activation(activations, col_size, row_size, act_index, cmap): 
-    activation = activations[act_index]
+def __display_activation(activation, title, col_size, row_size, cmap): 
+    
     activation_index=0
     fig, ax = plt.subplots(row_size, col_size, figsize=(row_size*13.5,col_size*2.5))
+    fig.suptitle(title, fontsize=150)
     for row in range(0,row_size):
         for col in range(0,col_size):
             ax[row][col].imshow(activation[0, :, :, activation_index], cmap=cmap)
